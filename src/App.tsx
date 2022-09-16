97a5a8573ae5c2cfe391883d0ce6cde0c2b97bb4
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-17 02:18:02 +0500

    provide ModalAddTask to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import Header from './components/Header';
-
+import BlueButton from './components/BlueButton';
+import ModalAddTask from './components/ModalAddTask';
 import Todos from './components/Todos';
 import { getTodosData } from './common/localStorage';
 import { useAppDispatch } from './hooks/redux';
 import { setTodos } from './store/reducers/todosSlice';
 
 const App = () => {
+  const [showAddModal, setShowModal] = useState(false);
   const dispatch = useAppDispatch();
+
+  const toggleShowAddModal = () => setShowModal(prevState => !prevState);
 
   //upload todos to global storage
   useEffect(() => {
@@ -23,6 +27,8 @@ const App = () => {
       <StatusBar backgroundColor={'transparent'} />
       <Header />
       <Todos />
+      <BlueButton onPress={toggleShowAddModal} innerText={'Добавить'} />
+      <ModalAddTask close={toggleShowAddModal} isShow={showAddModal} />
     </SafeAreaView>
   );
 };
