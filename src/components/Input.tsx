6140e97a37fe63239df6e329cc873792eaee1fe2
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-17 02:12:07 +0500

    new Component of Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+import type {
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native';
+import { BackgroundColor, TextColor } from '../constants/colors';
+
+type Props = {
+  value: string;
+  setValue: { (value: string): void };
+  placeholder?: string;
+};
+
+type InputChangeEvent = NativeSyntheticEvent<TextInputChangeEventData>;
+
+const Input = ({ value, setValue, placeholder }: Props) => {
+  let delayTimer: number;
+
+  const onChange = (ev: InputChangeEvent) => {
+    const text: string = ev.nativeEvent.text;
+    //Updating state after timer
+    delayTimer && clearTimeout(delayTimer);
+    delayTimer = setTimeout(() => setValue(text), 200);
+  };
+
+  return (
+    <TextInput
+      style={styles.container}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder || ''}
+      placeholderTextColor={TextColor.lightPlaceholder}
+      selectionColor={BackgroundColor.lightSystemBlue}
+    />
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 32,
+    width: '100%',
+
+    borderColor: BackgroundColor.lightBorder,
+    backgroundColor: BackgroundColor.white,
+    borderRadius: 7,
+    paddingHorizontal: 6,
+    paddingVertical: 7,
+
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 18,
+    letterSpacing: -0.078,
+  },
+});
