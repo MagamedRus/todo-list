19a174d29365d5eb1b874fe5452bf33784d96a74
Muhammad <muhammadkasymov@yandex.ru>
2022-09-16 17:57:41 +0500

    fix App import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
 import store from './src/store/index';
 
