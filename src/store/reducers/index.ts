d2c4744e77c4a437462814133113f857021ed782
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-16 17:07:32 +0500

    deleted redux

--- src/store/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import filterReducer from './filterReducer';
-
-const rootReducer = combineReducers({ filter: filterReducer });
-
-export default rootReducer;
